e1a053193169b3ad98b3ae428fd11631128d2a2f
Christian Trevino <ctrevino1994@yahoo.com>
2020-01-07 22:21:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,15 @@
 function basicTeenager(age) {
-
+if (13){
+  return "You are a teenager!"
+}
 }
 
 function teenager(age) {
-
+if (13) {
+   return "You are a teenager!"
+} else {
+  return "You are not a teenager"
+}
 }
 
 function ageChecker(age) {
