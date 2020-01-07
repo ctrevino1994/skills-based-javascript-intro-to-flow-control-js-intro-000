1a8fa9aece0ee38e0c9bf320c378529e564d49f2
Christian Trevino <ctrevino1994@yahoo.com>
2020-01-07 23:21:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,9 +14,9 @@ if (age > 12 && age < 20) {
 function ageChecker(age) {
 if (age > 12 && age < 20) {
   return "You are a teenager!"
-} else if (age >= 12) {
+} else if (age <= 12) {
   return "You are a kid"
-} else if (age <= 20) {
+} else if (age >= 20) {
   return "You are a grownup"
 }
 }
